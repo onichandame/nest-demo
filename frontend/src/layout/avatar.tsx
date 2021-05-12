8039234c1a4fcb81a873f33f5373e873286e93dc
onichandame <zxinmyth@gmail.com>
2021-05-12 08:01:48 +0000

    staged. login not working

--- frontend/src/layout/avatar.tsx
+++ frontend/src/layout/avatar.tsx
@@ -3,19 +3,27 @@ import { IconButton, Menu, MenuItem } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 import { v1 as createUid } from 'uuid'
 
+import { DialogProps } from '../types'
 import { Login, Logout } from '../components'
 import { User } from '../context'
+
+const LogDialog: FC<DialogProps> = args => {
+  const [user] = useContext(User)
+  if (user) return <Logout {...args} />
+  else return <Login {...args} />
+}
 
 export const Avatar: FC = () => {
   const [open, setOpen] = useState(false)
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
   const [menuId] = useState(createUid())
   const [user] = useContext(User)
-  const onClickedItem = () => {
-    setOpen(false)
-    setAnchorEl(null)
+  const onMenuItemClicked = () => setAnchorEl(null)
+  const onDialogOpen = () => {
+    setOpen(true)
+    onMenuItemClicked()
   }
-  const onMenuClose = () => setOpen(false)
+  const onDialogClose = () => setOpen(false)
   return (
     <div>
       <IconButton
@@ -35,21 +43,11 @@ export const Avatar: FC = () => {
         keepMounted
         onClose={() => setAnchorEl(null)}
       >
-        {!!user ? (
-          <div>
-            <MenuItem>logout</MenuItem>
-          </div>
-        ) : (
-          <div>
-            <MenuItem onClick={onClickedItem}>login</MenuItem>
-          </div>
-        )}
+        <MenuItem button onClick={onDialogOpen}>
+          {!!user ? `logout` : `login`}
+        </MenuItem>
       </Menu>
-      {!!user ? (
-        <Logout open={open} onClose={onMenuClose} />
-      ) : (
-        <Login open={open} onClose={onMenuClose} />
-      )}
+      <LogDialog open={open} onClose={onDialogClose} />
     </div>
   )
 }
